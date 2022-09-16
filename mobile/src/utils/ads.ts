9f2c4de02dd3e85ba5a33175c796419ed659c230
Natasha Matos <natasha.oliveirabusiness@gmail.com>
2022-09-15 18:17:28 -0700

    feat: created the ads screen Created application routes Stack navigation implemented

---
+++ mobile/src/utils/ads.ts
@@ -0,0 +1,25 @@
+export const ADS = [
+  {
+    id: '8541feec-41f7-4167-b88c-2a6277aceaba',
+    ads: [
+      {
+        id: '275162e2-ac68-416e-ba86-e7132deb7294',
+        name: 'André',
+        weekDays: ['0', '5', '6'],
+        useVoiceChannel: true,
+        yearsPlaying: 5,
+        hourStart: '18:00',
+        hourEnd: '23:00',
+      },
+      {
+        id: '1f886264-e090-4566-ae42-c7b2fca37dd0',
+        name: 'InnocentGirlQ',
+        weekDays: ['5', '6'],
+        useVoiceChannel: true,
+        yearsPlaying: 2,
+        hourStart: '18:00',
+        hourEnd: '22:00',
+      },
+    ],
+  },
+];
--- mobile/src/utils/games.ts
+++ mobile/src/utils/games.ts
@@ -1,38 +1,83 @@
 export const GAMES = [
   {
-    id: '1',
-    name: 'League of Legends',
-    ads: '4',
-    cover: require('../assets/games/game-1.png')
+    id: '643b3cad-f2a6-4e19-ac6d-c39684807353',
+    title: 'League of Legends',
+    bannerUrl: 'https://static-cdn.jtvnw.net/ttv-boxart/21779-285x380.jpg',
+    _count: {
+      ads: 0,
+    },
   },
   {
-    id: '2',
-    name: 'Dota 2',
-    ads: '3',
-    cover: require('../assets/games/game-2.png')
+    id: '46f46d52-1e7d-45c9-8d62-ccefcd0dc3bd',
+    title: 'Counter-Strike: Global Offensive',
+    bannerUrl: 'https://static-cdn.jtvnw.net/ttv-boxart/32399_IGDB-285x380.jpg',
+    _count: {
+      ads: 0,
+    },
   },
   {
-    id: '3',
-    name: 'CS-GO',
-    ads: '2',
-    cover: require('../assets/games/game-3.png')
+    id: '4579ccb7-9a26-425f-b166-76bec3ab883d',
+    title: 'VALORANT',
+    bannerUrl: 'https://static-cdn.jtvnw.net/ttv-boxart/516575-285x380.jpg',
+    _count: {
+      ads: 0,
+    },
   },
   {
-    id: '4',
-    name: 'Apex',
-    ads: '3',
-    cover: require('../assets/games/game-4.png')
+    id: 'c1ec50c7-9a7f-47ab-9e87-29589aaa9d53',
+    title: 'Grand Theft Auto V',
+    bannerUrl: 'https://static-cdn.jtvnw.net/ttv-boxart/32982_IGDB-285x380.jpg',
+    _count: {
+      ads: 0,
+    },
   },
   {
-    id: '5',
-    name: 'Fortnite',
-    ads: '5',
-    cover: require('../assets/games/game-5.png')
+    id: '87e66044-a78c-40ab-a1ae-5409a37b3f21',
+    title: 'Project Zomboid',
+    bannerUrl: 'https://static-cdn.jtvnw.net/ttv-boxart/31339_IGDB-285x380.jpg',
+    _count: {
+      ads: 0,
+    },
   },
   {
-    id: '6',
-    name: 'World of Warcraft',
-    ads: '2',
-    cover: require('../assets/games/game-6.png')
+    id: 'b0b85a74-9d56-47fa-97c7-abc8d09587e3',
+    title: 'Fortnite',
+    bannerUrl: 'https://static-cdn.jtvnw.net/ttv-boxart/33214-285x380.jpg',
+    _count: {
+      ads: 0,
+    },
+  },
+  {
+    id: '87e7afcc-b6aa-4364-84c6-f08eb9caba1d',
+    title: 'Minecraft',
+    bannerUrl: 'https://static-cdn.jtvnw.net/ttv-boxart/27471_IGDB-285x380.jpg',
+    _count: {
+      ads: 0,
+    },
+  },
+  {
+    id: '8541feec-41f7-4167-b88c-2a6277aceaba',
+    title: 'Tibia',
+    bannerUrl: 'https://static-cdn.jtvnw.net/ttv-boxart/19619_IGDB-285x380.jpg',
+    _count: {
+      ads: 2,
+    },
+  },
+  {
+    id: 'fddf93a2-ff45-435f-a164-0cd3dc89b1fc',
+    title: 'Dota 2',
+    bannerUrl: 'https://static-cdn.jtvnw.net/ttv-boxart/29595-285x380.jpg',
+    _count: {
+      ads: 0,
+    },
+  },
+  {
+    id: '46f32415-bde8-41b9-ab55-3d468b5d9f32',
+    title: 'FIFA 22',
+    bannerUrl:
+      'https://static-cdn.jtvnw.net/ttv-boxart/1869092879_IGDB-285x380.jpg',
+    _count: {
+      ads: 0,
+    },
   },
 ];
